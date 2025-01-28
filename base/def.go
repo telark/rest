cf5ce31f7ad6f76ff2afdc595874a8db7a033615
hourki <houssem.kraoua@gmail.com>
2025-01-28 13:06:18 +0100

    adjust rest utils and api structs

--- base/def.go
+++ base/def.go
@@ -1,16 +1,19 @@
 package base
 
 import (
+	"errors"
 	"fmt"
+
+	"github.com/plsyro/common-pkg/v2/global"
 )
 
-type RestAPI struct {
+type API struct {
 	Host        Host
-	APIVersion  APIVersion
+	Version     Version
 	Endpoint    Endpoint
 	ContentType ContentType
 	Payload     []byte
-	Method      string
+	Method      Method
 }
 
 type Host struct {
@@ -19,12 +22,13 @@ type Host struct {
 	Port    Port
 }
 
-type APIVersion string
+type Version string
 type Endpoint string
 type Schema string
 type Service string
 type ContentType string
 type Port int
+type Method string
 
 const (
 	HTTP  Schema = "http://"
@@ -32,9 +36,9 @@ const (
 )
 
 const (
-	CONFIGURATOR       Service = "plsyro-configurator-service"
-	EXPORTER           Service = "plsyro-exporter-service"
-	ADMISSION_OPERATOR Service = "plsyro-admission-operator-service"
+	CONFIGURATOR       Service = "configurator"
+	EXPORTER           Service = "exporter"
+	ADMISSION_OPERATOR Service = "admission-operator"
 )
 
 const (
@@ -47,9 +51,50 @@ const (
 )
 
 const (
-	V1 APIVersion = "api/v1"
+	V1 Version = "api/v1"
 )
 
-func (restApi *RestAPI) GenerateURL() string {
-	return fmt.Sprintf("%s%s:%d/%s/%s", restApi.Host.Schema, restApi.Host.Service, restApi.Host.Port, restApi.APIVersion, restApi.Endpoint)
+const (
+	GET    Method = "GET"
+	POST   Method = "POST"
+	UPDATE Method = "PUT"
+	DELETE Method = "DELETE"
+)
+
+func (api *API) GenerateURL() (string, error) {
+	if err := api.Validate(); err != nil {
+		return "", err
+	}
+
+	serviceName := GetServiceName(api.Host.Service)
+
+	if (api.Host.Schema == HTTP && api.Host.Port == 80) || (api.Host.Schema == HTTPS && api.Host.Port == 443) {
+		return fmt.Sprintf("%s%s/%s/%s", api.Host.Schema, serviceName, api.Version, api.Endpoint), nil
+	}
+	return fmt.Sprintf("%s%s:%d/%s/%s", api.Host.Schema, serviceName, api.Host.Port, api.Version, api.Endpoint), nil
+}
+
+func (api *API) Validate() error {
+	if api.Host.Schema == "" {
+		return errors.New("schema is required")
+	}
+	if api.Host.Service == "" {
+		return errors.New("service is required")
+	}
+	if api.Version == "" {
+		return errors.New("version is required")
+	}
+	if api.Endpoint == "" {
+		return errors.New("endpoint is required")
+	}
+	return nil
+}
+
+func GetServiceName(service Service) string {
+	prefix := global.BaseNamespace
+	if prefix == "" {
+		// Fallback to Default Namespace
+		prefix = "default"
+	}
+	return fmt.Sprintf("%s-%s-service", prefix, service)
 }
---
+++ utils/request/def.go
@@ -0,0 +1,25 @@
+package request
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func ParseRequestBody(r *http.Request) (map[string]interface{}, error) {
+	body, err := io.ReadAll(io.LimitReader(r.Body, 1<<20)) // 1 MB limit
+	if err != nil {
+		return nil, fmt.Errorf("failed to read request body: %w", err)
+	}
+	if len(body) == 0 {
+		return nil, errors.New("request body is empty")
+	}
+
+	var spec map[string]interface{}
+	if err := json.Unmarshal(body, &spec); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON: %w", err)
+	}
+	return spec, nil
+}
