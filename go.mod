c8634cdee12973cf955e18e65a8b8d7deb72c649
hourki <houssem.kraoua@gmail.com>
2025-01-28 12:38:38 +0100

    add rest utils to handle common funcs

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/plsyro/rest-pkg
 
 go 1.23.4
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/plsyro/common-pkg/v2 v2.6.0
+)
---
+++ utils/def.go
@@ -0,0 +1,46 @@
+package utils
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/plsyro/common-pkg/v2/logging"
+	"github.com/plsyro/rest-pkg/response"
+)
+
+var logger *logging.CustomLogger
+
+func init() {
+	logger = logging.NewCustomLogger("RestUtils: ")
+}
+
+func LogAndSendResponse(w http.ResponseWriter, status int, operation response.OperationStatus, message string, data interface{}, err error) {
+	logMessage(message, err)
+	sendResponse(w, status, operation, message, data)
+}
+
+func LogAndReturnResponse(w http.ResponseWriter, status int, operation response.OperationStatus, message string, data interface{}, err error) *response.GenericResponse {
+	logMessage(message, err)
+	return createGenericResponse(status, operation, message, data)
+}
+
+func logMessage(message string, err error) {
+	if err != nil {
+		logger.Error(fmt.Sprintf("%s: %v", message, err))
+	} else {
+		logger.Info(message)
+	}
+}
+
+func sendResponse(w http.ResponseWriter, status int, operation response.OperationStatus, message string, data interface{}) {
+	response.SendSingleResponse(w, createGenericResponse(status, operation, message, data))
+}
+
+func createGenericResponse(status int, operation response.OperationStatus, message string, data interface{}) *response.GenericResponse {
+	return &response.GenericResponse{
+		Status:    status,
+		Operation: string(operation),
+		Message:   message,
+		Data:      data,
+	}
+}
