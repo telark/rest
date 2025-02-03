21b26ddbad453c43fb50ea8b71e54a720616a645
hourki <houssem.kraoua@gmail.com>
2025-02-03 21:54:52 +0100

    adjust admissions EPs

--- endpoints/admissions/def.go
+++ endpoints/admissions/def.go
@@ -3,6 +3,9 @@ package admissions
 import "github.com/plsyro/rest-pkg/base"
 
 const (
+	// Create A
+	CREATE_ADMISSION_VALIDATION_WEBHOOK base.Endpoint = "admissions/validation/create"
+
 	// Validate Endpoints
 	VALIDATE_GROUPER_ADMISSION base.Endpoint = "admissions/groupers/{name}/validate"
 )
