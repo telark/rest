34caa6d5669da2eb2ce006bc768432403034e1e9
hourki <houssem.kraoua@gmail.com>
2025-04-22 10:11:04 +0200

    add generic CreateRoute Func

--- router/def.go
+++ router/def.go
@@ -1,9 +1,11 @@
 package router
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/plsyro/rest-pkg/base"
 )
 
 type Route struct {
@@ -26,4 +28,12 @@ func NewRouter(Routes []Route) *mux.Router {
 	}
 
 	return router
+}
+
+func CreateRoute(method base.Method, endpoint base.Endpoint, handlerFunc interface{}) Route {
+	return Route{
+		Method:     string(method),
+		Pattern:    fmt.Sprintf("/%s/%s", base.V1, endpoint),
+		HandleFunc: http.HandlerFunc(handlerFunc.(func(w http.ResponseWriter, r *http.Request))),
+	}
 }
