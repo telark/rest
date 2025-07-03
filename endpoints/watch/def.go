230af798ad5d22f3c8eb92e69055f690b40eabc7
hourki <houssem.kraoua@gmail.com>
2025-07-03 20:24:59 +0200

    add Watch EPs for workloads

--- endpoints/watch/def.go
+++ endpoints/watch/def.go
@@ -6,4 +6,10 @@ const (
 	// Groupers
 	WATCH_ALL_GROUPERS base.Endpoint = "resources/groupers/watch"
 	WATCH__GROUPER      base.Endpoint = "resources/groupers/{name}/watch"
+
+	// Workloads
+	WATCH_ALL_APPS_WORKLOADS    base.Endpoint = "resources/workloads/apps/watch"
+	WATCH_ALL_BATCHES_WORKLOADS base.Endpoint = "resources/workloads/batches/watch"
+	WATCH_APP_WORKLOAD          base.Endpoint = "resources/workloads/apps/{name}/watch"
+	WATCH_BATCH_WORKLOAD        base.Endpoint = "resources/workloads/batches/{name}/watch"
 )
