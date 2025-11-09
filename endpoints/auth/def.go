22ee3ce500e5238c72266b6dd6ff4d03b366a6a5
hourki <houssem.kraoua@gmail.com>
2025-11-09 20:00:35 +0100

    adjust auth EPs

--- endpoints/auth/def.go
+++ endpoints/auth/def.go
@@ -3,12 +3,25 @@ package auth
 import "github.com/plsyro/rest/base"
 
 const (
-	StartLogin     base.Endpoint = "auth/login/start"
-	FinishLogin    base.Endpoint = "auth/login/finish"
-	Logout         base.Endpoint = "auth/logout"
-	GetAllPasskeys base.Endpoint = "auth/passkeys/get"
-	GetPasskey     base.Endpoint = "auth/passkeys/{id}/get"
-	CreatePasskey  base.Endpoint = "auth/passkeys/create"
-	PatchPasskey   base.Endpoint = "auth/passkeys/{id}/patch"
-	DeletePasskey  base.Endpoint = "auth/passkeys/{id}/delete"
+	// Login & logout
+	StartLogin  base.Endpoint = "auth/login/start"
+	FinishLogin base.Endpoint = "auth/login/finish"
+	Logout      base.Endpoint = "auth/logout"
+
+	// Passkeys
+	GetAllPasskeysByUser base.Endpoint = "auth/passkeys/{userId}/get"
+	GetPasskeyByUser     base.Endpoint = "auth/passkeys/{userId}/{id}/get"
+	CreatePasskeyByUser  base.Endpoint = "auth/passkeys/{userId}/create"
+	PatchPasskeyByUser   base.Endpoint = "auth/passkeys/{userId}/{id}/patch"
+	DeletePasskeyByUser  base.Endpoint = "auth/passkeys/{userId}/{id}/delete"
+
+	// Sessions
+	CreateSessionByUser  base.Endpoint = "auth/sessions/{userId}/create"
+	GetSessionByToken    base.Endpoint = "auth/sessions/{token}/get"
+	DeleteSessionByToken base.Endpoint = "auth/sessions/{token}/delete"
+
+	// Challenges
+	CreateChallengeByUser base.Endpoint = "auth/challenges/{userId}/create"
+	GetChallengeByUser    base.Endpoint = "auth/challenges/{userId}/get"
+	DeleteChallengeByUser base.Endpoint = "auth/challenges/{userId}/delete"
 )
