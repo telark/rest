d4df0efbcab5fdeaac379df237ef7b041d3d2a7e
hourki <houssem.kraoua@gmail.com>
2025-11-11 15:03:31 +0100

    adjust users EPs

--- endpoints/resources/users/def.go
+++ endpoints/resources/users/def.go
@@ -5,8 +5,8 @@ import "github.com/plsyro/rest/base"
 const (
 	CreateUser        base.Endpoint = "resources/users/create"
 	GetAllUsers       base.Endpoint = "resources/users/get"
-	GetUserByID       base.Endpoint = "resources/users/{id}/get"
-	GetUserByUsername base.Endpoint = "resources/users/{username}/get"
+	GetUserByID       base.Endpoint = "resources/users/findbyid/{id}/get"
+	GetUserByUsername base.Endpoint = "resources/users/findbyusername/{username}/get"
 	PatchUserByID     base.Endpoint = "resources/users/{id}/patch"
 	DeleteUserByID    base.Endpoint = "resources/users/{id}/delete"
 )
