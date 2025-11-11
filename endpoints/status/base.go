798d7ada0c63d635e0488c073ae0057f3a1035ec
hourki <houssem.kraoua@gmail.com>
2025-11-11 21:40:02 +0100

    add k8s probes Eps in status check

--- endpoints/status/base.go
+++ endpoints/status/base.go
@@ -3,5 +3,7 @@ package status
 import "github.com/plsyro/rest/base"
 
 const (
-	HealthCheck base.Endpoint = "status/health"
+	HealthCheck    base.Endpoint = "status/health"
+	ReadinessCheck base.Endpoint = "status/ready"
+	LivenessCheck  base.Endpoint = "status/live"
 )
