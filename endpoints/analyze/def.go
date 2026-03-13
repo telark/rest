606baf40f34435a83929260cdda33ee85b12ae8c
hourki <houssem.kraoua@gmail.com>
2026-03-14 00:02:04 +0100

    add new analyze EP

--- endpoints/analyze/def.go
+++ endpoints/analyze/def.go
@@ -3,7 +3,8 @@ package analyze
 import "github.com/plsyro/rest/base"
 
 const (
-	Startanalyze base.Endpoint = "analyze/start"
-	GetAllWorkloadsByNamespace base.Endpoint = "analyze/workloads/{namespace}/get"
-	GetAllResourcesByNamespace base.Endpoint = "analyze/resources/{namespace}/get"
+	Startanalyze                base.Endpoint = "analyze/start"
+	GetAllWorkloadsByNamespace  base.Endpoint = "analyze/workloads/{namespace}/get"
+	GetAllResourcesByNamespace  base.Endpoint = "analyze/resources/{namespace}/get"
+	SearchAllResourcesOfApplication   base.Endpoint = "analyze/applications/search" // search by label or text
 )
