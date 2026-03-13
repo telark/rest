61cd8b623fc2fe1ff8027062c78cf2b3533bf6e8
hourki <houssem.kraoua@gmail.com>
2026-03-13 22:51:45 +0100

    add new analyze EPs for k8s

--- endpoints/analyze/def.go
+++ endpoints/analyze/def.go
@@ -4,4 +4,6 @@ import "github.com/plsyro/rest/base"
 
 const (
 	Startanalyze base.Endpoint = "analyze/start"
+	GetAllWorkloadsByNamespace base.Endpoint = "analyze/workloads/{namespace}/get"
+	GetAllResourcesByNamespace base.Endpoint = "analyze/resources/{namespace}/get"
 )
