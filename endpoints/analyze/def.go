4e6a3e8b90b2e8f7a890266a557475968d39932b
hourki <houssem.kraoua@gmail.com>
2026-03-14 22:43:38 +0100

    add new enrichment EP

--- endpoints/analyze/def.go
+++ endpoints/analyze/def.go
@@ -3,8 +3,7 @@ package analyze
 import "github.com/plsyro/rest/base"
 
 const (
-	Startanalyze                base.Endpoint = "analyze/start"
-	GetAllWorkloadsByNamespace  base.Endpoint = "analyze/workloads/{namespace}/get"
-	GetAllResourcesByNamespace  base.Endpoint = "analyze/resources/{namespace}/get"
-	SearchAllResourcesOfApplication   base.Endpoint = "analyze/applications/search" // search by label or text
+	Startanalyze               base.Endpoint = "analyze/start"
+	GetAllWorkloadsByNamespace base.Endpoint = "analyze/workloads/{namespace}/get"
+	GetAllResourcesByNamespace base.Endpoint = "analyze/resources/{namespace}/get"
 )
---
+++ endpoints/enrichment/def.go
@@ -0,0 +1,7 @@
+package enrichment
+
+import "github.com/plsyro/rest/base"
+
+const (
+	EnrichApplications base.Endpoint = "enrichment/applications" // search by label or text
+)
