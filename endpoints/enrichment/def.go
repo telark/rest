cfe8fd01d4347cbe845291fc465f327172d5b7ef
hourki <houssem.kraoua@gmail.com>
2026-03-16 21:47:27 +0100

    update EPs

--- endpoints/enrichment/def.go
+++
@@ -1,7 +0,0 @@
-package enrichment
-
-import "github.com/plsyro/rest/base"
-
-const (
-	EnrichApplications base.Endpoint = "enrichment/applications" // search by label or text
-)
--- endpoints/resources/applications/def.go
+++ endpoints/resources/applications/def.go
@@ -3,8 +3,9 @@ package applications
 import "github.com/plsyro/rest/base"
 
 const (
-	CreateApplication     base.Endpoint = "resources/applications/create"
-	GetAllApplications    base.Endpoint = "resources/applications/get"
+	EnrichApplications      base.Endpoint = "resources/applications/enrich" // namespace, selector params are optional
+	CreateApplication       base.Endpoint = "resources/applications/create"
+	GetAllApplications      base.Endpoint = "resources/applications/get"
 	GetApplicationByName    base.Endpoint = "resources/applications/{name}/get"
 	PatchApplicationByName  base.Endpoint = "resources/applications/{name}/patch"
 	DeleteApplicationByName base.Endpoint = "resources/applications/{name}/delete"
