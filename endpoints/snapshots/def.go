18acb18f2c8cd3deaf29ef39450604a07930e114
hourki <houssem.kraoua@gmail.com>
2026-03-19 20:52:27 +0100

    add snapshots EPs

---
+++ endpoints/snapshots/def.go
@@ -0,0 +1,8 @@
+package snapshots
+
+import "github.com/plsyro/rest/base"
+
+const (
+	CreateSnapshot base.Endpoint = "resources/snapshots/create"
+	GetSnapshot    base.Endpoint = "resources/snapshots/{id}/get"
+)
